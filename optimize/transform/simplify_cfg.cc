a899563fdd673f51ed919d31d33fa96d8e56a3f1
HiddenL0ve <1161527727@qq.com>
2024-12-16 18:33:54 +0800

    update

--- optimize/transform/simplify_cfg.cc
+++ optimize/transform/simplify_cfg.cc
@@ -8,14 +8,14 @@ void SimplifyCFGPass::Execute() {
 
 // 删除从函数入口开始到达不了的基本块和指令
 // 不需要考虑控制流为常量的情况，你只需要从函数入口基本块(0号基本块)开始dfs，将没有被访问到的基本块和指令删去即可
-void SimplifyCFGPass::EliminateUnreachedBlocksInsts(CFG *C) { 
-    //TODO("EliminateUnreachedBlocksInsts"); 
-     std::vector<bool> visited(C->block_map->size(), false);
+void SimplifyCFGPass::EliminateUnreachedBlocksInsts(CFG *C) {
+    // TODO("EliminateUnreachedBlocksInsts");
+    std::vector<bool> visited(C->block_map->size(), false);
     // 使用栈进行DFS遍历
     std::stack<LLVMBlock> stack;
-    stack.push(C->block_map->at(0));  // 从入口基本块开始
+    stack.push(C->block_map->at(0));    // 从入口基本块开始
     visited[0] = true;
-    
+
     while (!stack.empty()) {
         LLVMBlock current_block = stack.top();
         stack.pop();
@@ -30,21 +30,21 @@ void SimplifyCFGPass::EliminateUnreachedBlocksInsts(CFG *C) {
             }
         }
     }
-    
+
     // 遍历所有基本块，删除不可达的基本块和指令
     for (int i = 0; i < C->block_map->size(); ++i) {
         if (!visited[i]) {
             // 删除不可达基本块的指令
             LLVMBlock block_to_remove = C->block_map->at(i);
-            
+
             // 清空该基本块的指令列表
             block_to_remove->Instruction_list.clear();
-            
+
             // 删除该基本块
             C->block_map->erase(i);
             // 从控制流图中移除该基本块
             C->G[i].clear();
-            for (auto& succ_list : C->G) {
+            for (auto &succ_list : C->G) {
                 succ_list.erase(std::remove(succ_list.begin(), succ_list.end(), block_to_remove), succ_list.end());
             }
         }
